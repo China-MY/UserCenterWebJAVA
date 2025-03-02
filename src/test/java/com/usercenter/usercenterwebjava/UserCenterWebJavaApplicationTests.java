1448ea4395d4941c5e7fe9f59fadb591188ba4fb
myxz2004 <mingyuxuezhang@qq.com>
2025-03-02 17:39:53 +0800

    20250302-登录和注册接口-优化文件结构

--- src/test/java/com/usercenter/usercenterwebjava/UserCenterWebJavaApplicationTests.java
+++ src/test/java/com/usercenter/usercenterwebjava/UserCenterWebJavaApplicationTests.java
@@ -1,10 +1,8 @@
 package com.usercenter.usercenterwebjava;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
 
 @SpringBootTest
 class UserCenterWebJavaApplicationTests {
